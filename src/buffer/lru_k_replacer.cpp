146f44dc4675e86632479694fc2ae9b648596b09
NEVERLAND_mercury <132367188+NEVERLANDlpx@users.noreply.github.com>
2024-03-28 21:10:39 +0800

    Update lru_k_replacer.cpp

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -64,15 +64,16 @@ auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
      }
        if(flag) { 
        *frame_id=ind;
-       Remove(ind); 
+       //Remove(ind); modified
        }
     return flag;
   
 }
 
+#include <cstdio>
 void LRUKReplacer::RecordAccess(frame_id_t frame_id,AccessType access_type) {
   if(frame_id>replacer_size_)   throw std::runtime_error("Invalid frame_id: " + std::to_string(frame_id)); 
-    
+    //printf("%d\n",frame_id);
     auto iterator = node_store_.find(frame_id);  
     if(iterator == node_store_.end())
      {
@@ -86,6 +87,8 @@ void LRUKReplacer::RecordAccess(frame_id_t frame_id,AccessType access_type) {
 }
 
 void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+	//for(auto it:node_store_) printf("%d ",it.first);
+	//puts("");
      auto iterator = node_store_.find(frame_id);  
      if(iterator == node_store_.end())
     throw std::runtime_error("Invalid frame_id: " + std::to_string(frame_id)); 
@@ -112,6 +115,7 @@ void LRUKReplacer::Remove(frame_id_t frame_id) {
    
    //latch_.lock();
    node_store_[frame_id].history_.clear();
+   //printf("*\n");
    node_store_.erase(frame_id);
    curr_size_--;
    //latch_.unlock();
