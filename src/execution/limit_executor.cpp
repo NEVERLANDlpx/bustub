b4ae66ca75027ec02c0b87b22e3db9dc48373d75
NEVERLAND_mercury <132367188+NEVERLANDlpx@users.noreply.github.com>
2024-05-08 20:05:32 +0800

    Update limit_executor.cpp

--- src/execution/limit_executor.cpp
+++ src/execution/limit_executor.cpp
@@ -16,10 +16,26 @@ namespace bustub {
 
 LimitExecutor::LimitExecutor(ExecutorContext *exec_ctx, const LimitPlanNode *plan,
                              std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    //: AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx),
+      plan_(plan),
+      child_executor_(std::move(child_executor)) {}
 
-void LimitExecutor::Init() { throw NotImplementedException("LimitExecutor is not implemented"); }
+void LimitExecutor::Init() { 
+//throw NotImplementedException("LimitExecutor is not implemented"); 
+   child_executor_->Init();
+   count=0;
+}
 
-auto LimitExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto LimitExecutor::Next(Tuple *tuple, RID *rid) -> bool { 
+  if(count>=plan_->GetLimit()) return false;
+  if(child_executor_->Next(tuple,rid)) //success emit
+  {
+    count++;
+    return true;
+  }
+  return false;
+  //return false;   
+}
 
 }  // namespace bustub
