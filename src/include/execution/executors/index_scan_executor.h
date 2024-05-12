f40e4dbbbbbcc0b3517c83077dcd972b060efc6c
NEVERLAND_mercury <132367188+NEVERLANDlpx@users.noreply.github.com>
2024-05-12 15:16:21 +0800

    Update index_scan_executor.h

--- src/include/execution/executors/index_scan_executor.h
+++ src/include/execution/executors/index_scan_executor.h
@@ -44,12 +44,9 @@ class IndexScanExecutor : public AbstractExecutor {
  private:
   /** The index scan plan node to be executed. */
   const IndexScanPlanNode *plan_;
-  //add
-  const TableInfo *table_info_;
-  const IndexInfo *index_info_;
-  HashTableIndexForTwoIntegerColumn *htable_;
-  std::unique_ptr<TableIterator> iter_;
+  TableInfo *table_info_;
+  IndexInfo *index_info_;
   std::vector<RID> rids_;
-  std::vector<RID>::const_iterator rid_iter_{};
+  int idx_;
 };
 }  // namespace bustub
